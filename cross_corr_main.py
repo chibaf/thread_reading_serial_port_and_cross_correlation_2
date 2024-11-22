3ff0e7c8ac6556a74e805741686f374345bbccae
chibaf <chibaf@mac.com>
2024-11-23 03:41:11 +0900

    Add files via upload

--- cross_corr_main.py
+++ cross_corr_main.py
@@ -1,12 +1,14 @@
 import threading
 import queue,time
 import serial,sys
+import random
 import cross_corr_sub
 
 i=1
 q =queue.Queue()  # queue which stores a result of a thread
-x=0.
-th = threading.Thread(target=cross_corr_sub.cross_corr, args=(x,q),daemon=True)
+x=0
+y=0
+th = threading.Thread(target=cross_corr_sub.cross_corr, args=(x,y,q),daemon=True)
 th.start()
 print("start thread: "+str(i))
 #th.join()
@@ -15,10 +17,11 @@ while True:
     ix = q.get()
     print(ix)
     i=i+1
-    if i>5:
+    if i>11:
       break;
-    x=0.
-    th = threading.Thread(target=cross_corr_sub.cross_corr, args=(x,q),daemon=True)
+    x=random.randint(-5,5)
+    y=random.randint(-5,5)
+    th = threading.Thread(target=cross_corr_sub.cross_corr, args=(x,y,q),daemon=True)
     th.start()
     print("start thread: "+str(i))
 exit()
--- cross_corr_sub.py
+++ cross_corr_sub.py
@@ -9,43 +9,41 @@ def find_index(c):  # find index of maximum value
       break
   return im
   
-def curv0(i):
-  if 1<=i<25:
+def curv0(i,x):
+  if 1<=i<25+x:
     ret=0
-  elif 25<=i<35:
+  elif 25+x<=i<35+x:
     ret=np.sin((i-25)*np.pi/10.)
   else:
     ret=0.
   return ret
   
-def curv1(i):
-  if 45<=i<55:
+def curv1(i,x):
+  if 0<=i<55+x:
     ret=0
-  elif 55<=i<65:
+  elif 55+x<=i<65+x:
     ret=np.sin((i-55)*np.pi/10.)
   else:
     ret=0.
   return ret
 
-def cross_corr(x,q):
-  time.sleep(10)
+def cross_corr(x,y,q):
   i=0
   d1=np.empty(0)
   d2=np.empty(0)
   while True:
-    r0=curv0(i)
-    r1=curv1(i)
+    r0=curv0(i,x)
+    r1=curv1(i,y)
     if i<100:
       d1=np.append(d1,np.float32(r0))
       d2=np.append(d2,np.float32(r1))
       i=i+1
     else:
-      if len(d1)!=0:
-        c=1.0/(np.linalg.norm(d1)*np.linalg.norm(d2)) 
-        f1=np.fft.fft(d1)
-        f2=np.conjugate(np.fft.fft(d2))
-        ff=f1*f2
-        corrf=np.real(np.fft.ifft(ff))*c
-        ix=find_index(corrf)
-        break
+      c=1.0/(np.linalg.norm(d1)*np.linalg.norm(d2)) 
+      f1=np.fft.fft(d1)
+      f2=np.conjugate(np.fft.fft(d2))
+      ff=f1*f2
+      corrf=np.real(np.fft.ifft(ff))*c
+      ix=find_index(corrf)
+      break
   q.put(ix)
